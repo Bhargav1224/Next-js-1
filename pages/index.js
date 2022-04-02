a5c594c06afe74e12327260a20e1f31c820c1969
Bhargav <bhargav.reddy@sakhaglobal.com>
2022-04-02 16:47:21 +0530

    developed navigation programatically using router.push and building custom 404 page for the app

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function HomePage() {
+  const router = useRouter();
+  // on success placing the order , going to order page
+  const handleProductClick = () => {
+    console.log("Placing the order");
+    router.push("/products");
+    // router.replace("/products");
+  };
   return (
     <div>
       <h1>Home Page</h1>
@@ -11,6 +19,10 @@ function HomePage() {
       <br />
       <br />
       <Link href="/products">Product Page</Link>
+      <br />
+      <br />
+      <br />
+      <button onClick={handleProductClick}>Place Order</button>
     </div>
   );
 }
