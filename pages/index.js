41ac4cec833d8a3a0320469dcffc926e403d2be6
Bhargav <bhargav.reddy@sakhaglobal.com>
2022-04-02 16:32:36 +0530

    Link based routing is done

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
+      <Link href="/blog">
+        <a>Blog Page</a>
+      </Link>
+      <br />
+      <br />
+      <br />
+      <Link href="/products">Product Page</Link>
     </div>
   );
 }
